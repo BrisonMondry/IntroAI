70d3c5041b1b9e280da8c1b02e48608644f1fabc
BrisonMondry <mondrybr@gmail.com>
2018-07-11 18:17:33 -0400

    Found a better library, starting work with that

--- Learner.py
+++ Learner.py
@@ -1,2 +1,8 @@
-class FileReader:
-    filename = ''
+#
+# file = open("train-labels-idx1-ubyte.gz",'rb')
+# by = file.read()
+# print(int.from_bytes(by,byteorder="big",signed=False))
+#
+
+import struct
+
