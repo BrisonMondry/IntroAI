d236d6676ffd4013ca81476e2fb5ecce106d8a5c
BrisonMondry <mondrybr@gmail.com>
2018-07-12 14:39:54 -0400

    Gzip found and implemented. Training labels successfully read

--- Learner.py
+++ Learner.py
@@ -5,4 +5,22 @@
 #
 
 import struct
+import gzip
+
+train_lables = gzip.open('train-labels-idx1-ubyte.gz', mode='rb')
+#by = f.read()
+magic = struct.unpack('>i', train_lables.read(4))
+items = struct.unpack('>i', train_lables.read(4))
+numbers = []
+
+for i in range(0,items[0]):
+    numbers.append(struct.unpack('>b', train_lables.read(1))[0])
+
+for n in numbers:
+    if n < 0 or n > 9:
+        print('nope')
+
+print(magic, " ", items)
+
+
 
