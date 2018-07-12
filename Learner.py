dd76022fa4dd49813f8940c2ee764b04c89cd830
BrisonMondry <mondrybr@gmail.com>
2018-07-12 14:54:30 -0400

    did most of the pre-work for the images

--- Learner.py
+++ Learner.py
@@ -8,19 +8,31 @@ import struct
 import gzip
 
 train_lables = gzip.open('train-labels-idx1-ubyte.gz', mode='rb')
-#by = f.read()
-magic = struct.unpack('>i', train_lables.read(4))
-items = struct.unpack('>i', train_lables.read(4))
+train_images = gzip.open('train-images-idx3-ubyte.gz', mode='rb')
+
+magic_train_labels = struct.unpack('>i', train_lables.read(4))
+num_train_labels = struct.unpack('>i', train_lables.read(4))
 numbers = []
 
-for i in range(0,items[0]):
+magic_train_images = struct.unpack('>i', train_images.read(4))[0]
+num_train_images = struct.unpack('>i', train_images.read(4))[0]
+train_img_rows = struct.unpack('>i', train_images.read(4))[0]
+train_img_cols = struct.unpack('>i', train_images.read(4))[0]
+
+print('''magic number: {}
+number of images: {}
+rows: {}
+columns: {}\n'''.format(magic_train_images, num_train_images, train_img_rows, train_img_cols))
+
+
+for i in range(0, num_train_labels[0]):
     numbers.append(struct.unpack('>b', train_lables.read(1))[0])
 
 for n in numbers:
     if n < 0 or n > 9:
         print('nope')
 
-print(magic, " ", items)
+print(magic_train_labels, " ", num_train_labels)
 
 
 
